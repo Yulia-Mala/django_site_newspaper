b3958e6acd2e5d1f41b896873473e6a58417a2f2
Yulia <Mala>
2023-04-01 18:11:41 +0300

    settings updated

--- django_site_newspaper/settings.py
+++ django_site_newspaper/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wz^(na0w&&r(v1ya_(b_4m3b$y^o56thsyyr@2)vkd#clhi+he'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-wz^(na0w&&r(v1ya_(b_4m3b$y^o56thsyyr@2)vkd#clhi+he")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = []
 
@@ -78,10 +78,23 @@ WSGI_APPLICATION = 'django_site_newspaper.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# To use Neon with Django, you have to create a Project on Neon and specify the project connection settings in your settings.py in the same way as for standalone Postgres.
+
+# DATABASES = {
+#   'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'NAME': 'neondb',
+#     'USER': 'Yulia-Mala',
+#     'PASSWORD': 'V3pzlHWD2oiO',
+#     'HOST': 'ep-sweet-field-250504.eu-central-1.aws.neon.tech',
+#     'PORT': '5432',
+#   }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -138,3 +151,9 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
